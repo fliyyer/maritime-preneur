e529d130782cb013e0889defb80c15525dc808dd
Marselina Rahmawati <marselina.rahmawati2046@students.unila.ac.id>
2023-03-02 10:09:30 +0700

    Header text responsive

--- src/component/Homepage/Header.jsx
+++ src/component/Homepage/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <header className="bg-image py-4">
             <div className="container mx-auto flex justify-between items-center px-4 h-96">
                 <div>
-                    <h1 className="text-white text-7xl font-bold mt-40">Maritimepreneur</h1>
+                    <h1 className="text-white text-4xl md:text-7xl sm:text-5xl font-bold mt-40">Maritimepreneur</h1>
                     <p className="text-white mt-8 font-normal text-3xl">
                         Building the Nations,<br /> Sustaining Our Oceans
                     </p>
