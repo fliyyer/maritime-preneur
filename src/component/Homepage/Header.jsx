4f06a764506f2d93609cd79eca532a2ac34f3fc2
Marselina Rahmawati <marselina.rahmawati2046@students.unila.ac.id>
2023-03-03 15:36:21 +0700

    Margin Header

--- src/component/Homepage/Header.jsx
+++ src/component/Homepage/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
         <header className="bg-image py-4">
             <div className="container mx-auto flex justify-between items-center px-4 h-96">
                 <div>
-                    <h1 className="text-white text-4xl md:text-7xl sm:text-5xl font-bold mt-40">Maritimepreneur</h1>
-                    <p className="text-white mt-8 font-normal text-3xl">
+                    <h1 className="text-white text-4xl md:text-7xl sm:text-5xl font-bold mt-40 ml-5">Maritimepreneur</h1>
+                    <p className="text-white mt-8 font-normal text-3xl ml-5">
                         Building the Nations,<br /> Sustaining Our Oceans
                     </p>
-                    <button className='bg-secondary-color w-52 h-14 rounded-xl mt-8 text-white font-bold hover:opacity-70'>Get Started</button>
+                    <button className='bg-secondary-color ml-5 w-52 h-14 rounded-xl mt-8 text-white font-bold hover:opacity-70'>Get Started</button>
 
                 </div>
             </div>
