1b175f6f16a329c9782167a9990332c7b51b0fbc
Marselina Rahmawati <marselina.rahmawati2046@students.unila.ac.id>
2023-03-09 10:57:40 +0700

    fix Service Spacing

--- src/component/Servicepage/OurService.jsx
+++ src/component/Servicepage/OurService.jsx
@@ -16,10 +16,10 @@ const OurService = () => {
 
     return (
         <div>
-            <div className="flex flex-wrap justify-center items-center bg-cover bg-center min-h-screen md:px-20 p-1 sm:p-6">
+            <div className="flex -mt-10 flex-wrap justify-center items-center bg-cover bg-center min-h-screen md:px-20 p-1 sm:p-6">
                 <div className="flex flex-col w-full lg:w-1/2 p-8 text-primary-color" data-aos='fade-up' data-aos-duration='1500'>
-                    <h2 className="text-center text-2xl lg:text-left font-bold sm:text-4xl mb-4">Digital Technology</h2>
-                    <p className="text-center text-m lg:text-left font-medium sm:text-xl mb-4">We provide services to develop and manage</p>
+                    <h2 className="text-center text-2xl lg:text-left font-bold sm:text-4xl mb-4 lg:mt-20">Digital Technology</h2>
+                    <p className="text-center text-m lg:text-left font-medium sm:text-xl mb-4">We Provide Services to Develop and Manage</p>
                     <hr />
                     <ul className="text-l mt-4 sm:text-2xl bg-white font-medium text-center lg:text-left  lg:list-disc mb-8 md:px-12 rounded-xl">
                         <li className='mb-2'>Mobile Apps</li>
@@ -41,9 +41,9 @@ const OurService = () => {
                 <div className="w-full lg:w-1/2 p-8" data-aos='zoom-in-up' data-aos-duration='1000'>
                     <img src={Service2} alt="Gambar Konten" className="w-full h-auto rounded-lg hidden lg:block " />
                 </div>
-                <div className="flex flex-col w-full lg:w-1/2 p-8 text-primary-color event-mt" data-aos='fade-up' data-aos-duration='1500'>
-                    <h2 className="text-center text-2xl lg:text-left font-bold sm:text-4xl md:-mt-20 mb-4 lg:mt-0">Event Organizer</h2>
-                    <p className="text-center text-m lg:text-left font-medium sm:text-xl mb-4">We provide services to organize</p>
+                <div className="flex mt-8 flex-col w-full lg:w-1/2 p-8 text-primary-color event-mt lg:mt-0" data-aos='fade-up' data-aos-duration='1500'>
+                    <h2 className="text-center text-2xl lg:text-left font-bold sm:text-4xl md:-mt-20 mb-4 lg:mt-20">Event Organizer</h2>
+                    <p className="text-center text-m lg:text-left font-medium sm:text-xl mb-4">We Provide Services to Organize</p>
                     <hr />
                     <ul className="text-l mt-4 sm:text-2xl bg-white font-medium mb-8 md:px-12 text-center lg:text-left  lg:list-disc">
                         <li className='mb-2'>Meeting</li>
@@ -60,10 +60,10 @@ const OurService = () => {
                     </ul>
                 </div>
             </div>
-            <div className="flex flex-wrap justify-center p-1 sm:p-6 -mt-10 lg:-mt-12 consulting-mt md:px-20 items-center lg:py-24" >
+            <div className="flex flex-wrap justify-center p-1 sm:p-6 -mt-10 lg:-mt-20 consulting-mt md:px-20 items-center lg:py-24" >
                 <div className="flex flex-col w-full lg:w-1/2 p-8 text-primary-color" data-aos='fade-up' data-aos-duration='1500'>
-                    <h2 className="text-center text-2xl lg:text-left font-bold sm:text-4xl mb-4">Consulting</h2>
-                    <p className="text-center text-m lg:text-left font-medium sm:text-xl mb-2">We provide consultancy in
+                    <h2 className="text-center text-2xl lg:text-left font-bold md:mt-10 sm:text-4xl mb-4 lg:-mt-10">Consulting</h2>
+                    <p className="text-center text-m lg:text-left font-medium sm:text-xl mb-2">We Provide Consultancy in
                     </p>
                     <hr />
                     <ul className="text-l mt-4 sm:text-2xl font-medium bg-white text-center lg:text-left  lg:list-disc mb-8 md:px-12">
@@ -74,19 +74,19 @@ const OurService = () => {
                         <li className='my-4'>Creative Economy</li>
                     </ul>
                 </div>
-                <div className="w-full lg:w-1/2 p-8" data-aos='zoom-in-up' data-aos-duration='1000'>
+                <div className="w-full lg:w-1/2 p-8 lg:-mt-10" data-aos='zoom-in-up' data-aos-duration='1000'>
                     <img src={Service3} alt="Gambar Konten" className="w-full h-auto rounded-lg hidden lg:block " />
                 </div>
             </div>
             <div className="flex flex-wrap justify-center items-center -mt-96 sm:-mt-64 md:-mt-80 lg:-mt-12 p-1 sm:p-6 bg-cover bg-center md:px-20 min-h-screen lg:py-24">
-                <div className="w-full lg:w-1/2 p-8" data-aos='zoom-in-up' data-aos-duration='1000'>
+                <div className="w-full lg:w-1/2 p-8 lg:-mt-10" data-aos='zoom-in-up' data-aos-duration='1000'>
                     <img src={Service4} alt="Gambar Konten" className="w-full h-auto rounded-lg hidden lg:block " />
                 </div>
-                <div className="flex flex-col w-full lg:w-1/2 p-8 text-primary-color edu-mt" data-aos='fade-up' data-aos-duration='1500'>
-                    <h2 className="text-center text-2xl lg:text-left font-bold sm:text-4xl mb-4">Education & Training</h2>
-                    <p className="text-center text-m lg:text-left font-medium sm:text-xl mb-2">We provide services for education & training</p>
+                <div className="flex flex-col w-full lg:w-1/2 p-8 text-primary-color edu-mt md:mt-0 -mt-96 lg:-mt-20" data-aos='fade-up' data-aos-duration='1500'>
+                    <h2 className="text-center -mt-40 text-2xl lg:text-left font-bold md:mt-10 sm:text-4xl mb-4">Education & Training</h2>
+                    <p className="text-center text-m lg:text-left font-medium sm:text-xl mb-2 ">We Provide Services for Education & Training</p>
                     <hr />
-                    <ul className="text-l mt-4 sm:text-2xl font-medium p-2 md:p-6 bg-white text-center lg:text-left  lg:list-disc md:px-12 ">
+                    <ul className="text-l sm:text-2xl font-medium p-2 md:p-6 bg-white text-center lg:text-left  lg:list-disc md:px-12 ">
                         <li className='my-3'>Youth Development</li>
                         <li className='mb-3'>Business Incubation</li>
                         <li className='mb-3'>Professional Certification</li>
